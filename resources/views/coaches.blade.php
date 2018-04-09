e6a8b656f6fb0116e0b54e8cf0abdbb19a092838
Aljona Orlova <aljona.orlova@bpw-consulting.com>
2018-04-09 14:44:17 +0300

    Fix

--- resources/views/coaches.blade.php
+++ resources/views/coaches.blade.php
@@ -15,7 +15,7 @@
     				<div class="col-md-3 col-sm-6 c-margin-b-30">
     					<div class="c-content-person-1 c-option-2">
     			  			<div class="c-caption c-content-overlay">
-    			  				<img class="c-overlay-object img-responsive" src="{{url($coach->picture)}}" alt="">
+    			  				<img class="c-overlay-object img-responsive" src="{{\Storage::url($coach->picture)}}" alt="">
     			  			</div>
     			  			<div class="c-body">
     				  			<div class="c-head">
