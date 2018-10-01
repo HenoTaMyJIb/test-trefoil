1cbc270f559d7592141ebe63fcf092e9f91dfbe3
Aljona Orlova <aljona.orlova@bpw-consulting.com>
2018-10-01 19:05:42 +0300

    Laravel and voyager update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,21 +123,7 @@
                 </a>
             </div>
         </div>
-        <!-- BEGIN: LAYOUT/FOOTERS/FOOTER-6 -->
-        <footer class="c-layout-footer c-layout-footer-6 c-bg-grey-1" style="padding-top: 0">
-            <div class="c-postfooter c-bg-dark-2">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 col-sm-12 c-col">
-                            <p class="c-copyright c-font-grey">{{date('Y')}} &copy; {{setting('site.title')}}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </footer>
+        
     </div>
     <!-- END: LAYOUT/FOOTERS/FOOTER-6 -->
 </body>
